4eb165e7473977edaede93d866092b88974adfb7
HwOdust <todka05@gmail.com>
2025-05-24 04:38:35 +0900

    DB table 만들기~~
    실험 아직 안 함~~(사유: 쿼리문 써야 실험 가능)

--- news/models.py
+++ news/models.py
@@ -1,2 +1,8 @@
 from django.db import models
 
+class News(models.Model):
+    News_title=models.CharField(max_length=200)
+    News_summary=models.TextField(blank=True)
+    News_content=models.TextField()
+    News_day=models.DateTimeField()
+    News_url=models.URLField(unique=True)
