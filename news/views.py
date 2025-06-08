9d5ec38f229eaaa5d03d581c84793d543c169b48
surcrious1 <kimes1923@gmail.com>
2025-06-08 23:07:17 +0900

    url, view 수정

--- news/views.py
+++ news/views.py
@@ -20,24 +20,6 @@ def test3(request):
 def main(request):
     return render(request,'news/mainpage.html')
 
-def search(request):
-    keyword = request.GET.get('keyword')
-    
-    if keyword:
-        keyword = keyword.strip()
-        headlines = News.objects.filter(News_title__icontains=keyword)
-    else:
-        headlines = []
-    
-    return render(request, 'news/resultpage.html', {
-        'keyword': keyword or '',
-        'headlines': headlines
-    })
-
-def detail(request, news_id):
-    article = get_object_or_404(News, pk=news_id)
-    return render(request, 'news/newspage.html', {'article': article})
-
 def index(request):
     return render(request,'index.html')
 
